889807fce49f344c3f559b1525de180f8105b8e2
Arik59 <kruftik203@gmail.com>
2024-11-16 16:41:34 +0200

    added UTCtime

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function updateCLock() {
+function updateCLock(time) {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
+    const hours = String(now.getUTCHours() + time).padStart(2, "0");
     const minutes = String(now.getUTCMinutes()).padStart(2, "0");
     const seconds = String(now.getUTCSeconds()).padStart(2, "0");
     document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+
+    const timeList = document.getElementById("timeList");
+    timeZone = parseInt(timeList.value, 10);
+    setInterval(updateCLock(timeZone), 1000);
 }
 
-setInterval(updateCLock, 1000);
+let timeZone = 0;
+
+timeList.addEventListener('change', updateCLock(timeZone))
+
+
+
+// const timeDifference = new Date().getTimezoneOffset() / 60;
+// const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// const timeZone2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
+// document.getElementById('clock2').textContent = timeZone;
