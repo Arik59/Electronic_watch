81ef34cc4479b7d3ccca5178d939bed8cd76005f
Arik59 <kruftik203@gmail.com>
2024-11-16 15:31:51 +0200

    added Time output

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function updateCLock() {
-
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-document.getElementById("clock").textContent = new Date();
+setInterval(updateCLock, 1000);
